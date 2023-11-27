4dd6d743db61c1b753103cadee237cee1627179b
vishal soni <vsoni23432@gmail.com>
2023-11-27 19:30:24 +0530

    add: apping

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
       <Router>
         <Navbar />
         <Routes>
-          <Route exact path='/' element={<Home/>} />
+          <Route path='/' element={<Home/>} />
           <Route
             path='/*'
             element={
