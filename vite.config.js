a82088c15676401f3e07be95e8deac2f34a103e7
vishal soni <vsoni23432@gmail.com>
2023-11-27 18:31:25 +0530

    add: packages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  base:"/vishal_soni_portfolio/git "
+  base:"/vishal_soni_portfolio/"
 })
