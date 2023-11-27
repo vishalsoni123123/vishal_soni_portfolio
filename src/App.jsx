6b77144e2fb7faa611be6c327054d6b587a00e7e
vishal soni <vsoni23432@gmail.com>
2023-11-27 16:50:32 +0530

    add: fixed

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,15 @@ const App = () => {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='./' element={<Home />} />
+          <Route path='/' element={<Home />} />
           <Route
-            path='./*'
+            path='/*'
             element={
               <>
                 <Routes>
-                  <Route path='./about' element={<About />} />
-                  <Route path='./projects' element={<Projects />} />
-                  <Route path='./contact' element={<Contact />} />
+                  <Route path='/about' element={<About />} />
+                  <Route path='/projects' element={<Projects />} />
+                  <Route path='/contact' element={<Contact />} />
                 </Routes>
                 <Footer />
               </>
